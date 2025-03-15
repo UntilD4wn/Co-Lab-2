62b724412adbac872416dae10d82d4473ea5491e
UntilD4wn <bethjlrowland@gmail.com>
2025-03-15 15:33:33 +0000

    V1

---
+++ Co-Lab2/Assets/Scripts/SceneSelecter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelecter : MonoBehaviour
+{
+    public void Level1()
+    {
+        //Play Game after clicking button
+        SceneManager.LoadSceneAsync(1);
+        Debug.Log("Change Scene Operational");
+    }
+
+    public void Level2()
+    {
+        //Play Game after clicking button
+        SceneManager.LoadSceneAsync(2);
+        Debug.Log("Change Scene Operational");
+    }
+}
